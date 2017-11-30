40b0b32ec27f09d3694183bc6e9cbb66074b2e9d
Students <khasperbali@gmail.com>
2017-11-29 14:39:14 -1000

    work on this

---
+++ apps.js
@@ -0,0 +1,15 @@
+var hero = "Zak"; //v
+var gender = "Genderless Heterosexual Homosexual Attack Helicopter"; //w
+var age = 18; //x
+var weapon = "Tape"; //y
+var pronoun =""; //z
+function age(){
+	output = "is just beginning";
+}
+if (age > 18 && 40 > age) {
+	output = "has seen a few battles";
+}
+else if (age > 40) {
+	output = "has lived through many epic battles";
+}
+console.log(hero, "is our lovable hero, who", age, "with his trusty", weapon);
